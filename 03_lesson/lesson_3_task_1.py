cfde0f87af67617f091f6be47c419bd576126ff2
Sergey <mr.zhadeev96@mail.ru>
2025-12-03 14:05:24 +0400

    .

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,9 @@
+from user import User
+
+# Создаем новый экземпляр User
+my_user = User("Иван", "Иванов")
+
+# Вызываем все методы
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()

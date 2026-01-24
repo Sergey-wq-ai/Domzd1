9af160627ec7d7a60c110511542327d7d3e35cdc
Sergey <mr.zhadeev96@mail.ru>
2026-01-24 21:16:44 +0400

    домашка

---
+++ 08_lesson/variables.py
@@ -0,0 +1,3 @@
+URL_NAME = "https://ru.yougile.com/api-v2/projects/"
+TOKEN_NAME = "Bearer -IpDh+n3-hSl2D9ipQCb8ZPEMxVxZwvxBdm0QP2rtme0rK8gBK+gYv5IVbU6lIFyi"
+PROJECT_ID = 'c4490052-50d6-41c3-9034-1e770862485d"'
